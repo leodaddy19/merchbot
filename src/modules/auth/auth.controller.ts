f55172bf3c1a593d9cde31111d36ee746c985f08
hungvt <hungvt@lattehub.com>
2021-06-25 15:02:13 +0700

    allow cors

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import { AuthGuard } from '@nestjs/passport';
 import { FBAuthReponseDto } from "./dtos/fb_auth_reponse.dto";
-@Controller('auth')
+@Controller('api/auth')
 export class AuthController {
   @UseGuards(AuthGuard('facebook-token'))  
   @Post('facebook')
